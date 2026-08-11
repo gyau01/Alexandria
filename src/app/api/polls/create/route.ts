c467a65b6bbb284889b3f278fa0f9f91911a0612
Yau <120261971+gyau01@users.noreply.github.com>
2026-08-11 18:13:33 -0400

    Vercel deploy edit_2

--- src/app/api/polls/create/route.ts
+++ src/app/api/polls/create/route.ts
@@ -11,38 +11,29 @@ export async function POST(req: Request){
 	}
 	
 	const {created_by, title, description, poll_type, options } = await req.json();
-
-
 if (typeof title !== "string" || !title.trim()) {
   return NextResponse.json({ error: "title is not valid" }, { status: 400 });
 }
-
 let cleanDescription: string | null = null;
-
 if (description != null ) {
 	if (typeof description !== "string") {
   	return NextResponse.json({ error: "description is invalid" }, { status: 400 });
   }
-
 	if (description.trim() === ""){
 		cleanDescription = null;
 	}
 	else {
 		cleanDescription = sanitizeHtml(description, {allowedTags: [], allowedAttributes: {},}).trim();
 	}
-
 }
-
 if (!Array.isArray(options) || options.length < 2 || options.length > 20 ){
 		return NextResponse.json({error: " options are not valid"}, {status: 400});
 	}
 	const cleanTitle = sanitizeHtml(title, {allowedTags: [], allowedAttributes: {} });
-
 const cleanOptions = options
 	.map((opt: unknown) => typeof opt === "string" ? sanitizeHtml(opt, { allowedTags: [], allowedAttributes: {} })
 	.trim(): "" )
 	.filter((opt: string) => opt.length > 0);
-
 	if (cleanOptions.length < 2) {
     return NextResponse.json({ error: "Please provide at least 2 valid options" }, { status: 400 });
   }
@@ -50,44 +41,34 @@ const cleanOptions = options
 		acc[option] = 0;
     return acc;
   }, {});
-
 	const { data: check, error: usageError } = await supabase
 		.from("users")
 		.select("subscription")
-		.eq("user_id", user.id)
+		.eq("id", user.id)
 		.single();
 	
-
 	if (usageError || !check ){
 		console.error("error checking the usage table")
 		return NextResponse.json({ error: "Error checking sub level" }, { status: 400 });
 	}
-
 	const sub_id = check.subscription;
-
 	const bit = 1 << 2;
-
 	if ((sub_id & bit)===0){
-
 		const { data: usage, error: use_err } = await supabase.from("user_usage")
 			.select("polls_usage")
 			.eq("user_id", user.id)
 			.single();
-
 		if ( use_err || !usage ) {
 			return NextResponse.json({error: " Error checking usage table"}, {status: 400});
 		}
 		
-		const { poll_usage } = usage;
-
-		if ( poll_usage == 0 ) {
+		const { polls_usage } = usage;
+		if ( polls_usage == 0 ) {
 			return NextResponse.json({error: " You are out of usage cases, please subscribe for more "}, { status: 401 } );
 		}
 		else {
-			let new_usage = poll_usage - 1;
-
+			let new_usage = polls_usage - 1;
 			await supabase.from("user_usage").update({polls_usage: new_usage}).eq("user_id",user.id).single();
-
 		}
 	}
 		
@@ -99,11 +80,8 @@ const cleanOptions = options
 		options: cleanOptions,
 		votes: initialVotes,
 		}).select().single();
-
 	if (err){
 		return NextResponse.json({ error: "hey its me" }, { status: 500 });
 	}
-
 	return NextResponse.json({ ok:true })
-
 }
